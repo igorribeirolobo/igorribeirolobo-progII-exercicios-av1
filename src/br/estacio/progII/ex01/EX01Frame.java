ee0e17cdeb66f180d0fa0a866d8b1de95048f5a9
Igor Ribeiro Lobo <igorribeirolobo@gmail.com>
2017-08-22 22:16:20 -0300

    botao
    faltando funcao botao

--- src/br/estacio/progII/ex01/EX01Frame.java
+++ src/br/estacio/progII/ex01/EX01Frame.java
@@ -1,6 +1,39 @@
 package br.estacio.progII.ex01;
-
-public class EX01Frame {
-
+import javax.swing.*; 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+public class EX01Frame extends JFrame {
+ 
+	public JButton calc = new JButton("Calcular");
+	public JTextField tab = new JTextField();
+    public JTextField de = new JTextField();
+    public JTextField ate = new JTextField();
+    public JFrame principal = new JFrame();
 	
+    public void Janela()
+	{
+		
+		principal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    principal.setTitle("Exercício 01");
+	    principal.setSize(400,250);
+	    principal.setVisible(true);	
+	    JLabel lbltab = new JLabel("Tabuada de: ");
+	    JLabel lblde = new JLabel(" Começo: ");
+	    JLabel lblate = new JLabel(" Final: ");
+	    Box Horizontal = Box.createHorizontalBox();
+	    Horizontal.add(lbltab);
+	    Horizontal.add(tab);
+	    Horizontal.add(lblde);
+	    Horizontal.add(de);
+	    Horizontal.add(lblate);
+	    Horizontal.add(ate);
+	    Horizontal.add(calc);
+	    principal.add(Horizontal,BorderLayout.PAGE_START);
+	    principal.setVisible(true);
+	}
+	
+   
+       
 }
